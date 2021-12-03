788e33e414b97a5e9d4fdc65e0dc78ae78f82d20
Giang <Giang@DESKTOP-DPREPJL>
2021-12-04 00:23:34 +0700

    Added fully methods

--- Caculator/Caculator/MainWindow.xaml.cs
+++ Caculator/Caculator/MainWindow.xaml.cs
@@ -20,7 +20,9 @@ namespace Caculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        double lastNumber, result; 
+        double lastNumber, result = 0;
+        SelectedOperator selectedOperation;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,7 +35,26 @@ namespace Caculator
 
         private void EqualBtn_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperation)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Subtraction:
+                        result = SimpleMath.Sub(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Mutilple(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Division(lastNumber, newNumber);
+                        break;
+                }
+                resultLabel.Content = result.ToString();
+            }
         }
 
         private void PercentBtn_Click(object sender, RoutedEventArgs e)
@@ -63,47 +84,48 @@ namespace Caculator
         {
             int selectedValue = 0;
 
-            if(sender == zeroBtn)
-            {
-                selectedValue = 0;
-            }
-            if (sender == oneBtn)
-            {
-                selectedValue = 1;
-            }
-            if (sender == twoBtn)
-            {
-                selectedValue = 2;
-            }
-            if (sender == threeBtn)
-            {
-                selectedValue = 3;
-            }
-            if (sender == fourBtn)
-            {
-                selectedValue = 4;
-            }
-            if (sender == fiveBtn)
-            {
-                selectedValue = 5;
-            }
-            if (sender == sixBtn)
-            {
-                selectedValue = 6;
-            }
-            if (sender == sevenBtn)
-            {
-                selectedValue = 7;
-            }
-            if (sender == eightBtn)
-            {
-                selectedValue = 8;
-            }
-            if (sender == nineBtn)
-            {
-                selectedValue = 9;
-            }
+            //if(sender == zeroBtn)
+            //{
+            //    selectedValue = 0;
+            //}
+            //if (sender == oneBtn)
+            //{
+            //    selectedValue = 1;
+            //}
+            //if (sender == twoBtn)
+            //{
+            //    selectedValue = 2;
+            //}
+            //if (sender == threeBtn)
+            //{
+            //    selectedValue = 3;
+            //}
+            //if (sender == fourBtn)
+            //{
+            //    selectedValue = 4;
+            //}
+            //if (sender == fiveBtn)
+            //{
+            //    selectedValue = 5;
+            //}
+            //if (sender == sixBtn)
+            //{
+            //    selectedValue = 6;
+            //}
+            //if (sender == sevenBtn)
+            //{
+            //    selectedValue = 7;
+            //}
+            //if (sender == eightBtn)
+            //{
+            //    selectedValue = 8;
+            //}
+            //if (sender == nineBtn)
+            //{
+            //    selectedValue = 9;
+            //}
 
+            selectedValue = int.Parse((sender as Button).Content.ToString());
 
             if (resultLabel.Content.ToString() == "0")
             {
@@ -116,12 +138,75 @@ namespace Caculator
             
         }
 
+        private void dotBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Contains("."))
+            {
+                resultLabel.Content = resultLabel.Content;
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content.ToString()}.";
+            }
+            
+        }
+
         private void OperationBtn_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
             {
                 resultLabel.Content = "0";
             }
+            if (sender == AddBtn)
+            {
+                selectedOperation = SelectedOperator.Addition;
+            }
+            if (sender == subBtn)
+            {
+                selectedOperation = SelectedOperator.Subtraction;
+            }
+            if (sender == multiBtn)
+            {
+                selectedOperation = SelectedOperator.Multiplication;
+            }
+            if (sender == divisionBtn)
+            {
+                selectedOperation = SelectedOperator.Division;
+            }
         }
+
+        
+    }
+    
+}
+public enum SelectedOperator
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+}
+
+public class SimpleMath
+{
+    public static double Add(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+    public static double Sub(double n1, double n2)
+    {
+        return n1 - n2;
+    }
+    public static double Mutilple(double n1, double n2)
+    {
+        return n1 * n2;
+    }
+    public static double Division(double n1, double n2)
+    {
+        if (n2 == 0)
+        {
+            MessageBox.Show("Divide 0 is not supported", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        return n1/n2; 
     }
 }
